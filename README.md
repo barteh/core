4be19a830b10b84de4b7385d6b18fb227e5a819c
ahad rafat talebi <rafat@bornamehr.com>
2018-08-15 02:36:14 +0430

    withTitle changed - read me updated

--- README.md
+++ README.md
@@ -3,3 +3,50 @@
 [![Build Status](https://travis-ci.org/barteh/core.svg?branch=master)](https://travis-ci.org/barteh/core)
 
 ## common component and libraries used for barteh based projects
+
+
+## Feature list
+
+###  a. common services
+1. geo.
+2. orgchart.
+3. unit.
+4. useraction.
+
+### b. react components
+1. [withTitle](#withTitle).
+2. error.
+3. user.
+4. withMessage.
+
+# withTitle
+
+adds title bar to on a component. use it for main forms and pages.
+
+### example:
+```js
+import React, { Component } from 'react';
+import Home from '@material-ui/icons/Home';
+
+class README extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default README;
+
+withTitle({
+    title:"page title",
+    subTitle:"page SubTitle",
+    description:"page description",
+    icon:<Home color="primary" style={{fontStyle:"36px"}}/>
+})(README)
+
+```
+
+
--- src/react/withTitle/withTitle.js
+++ src/react/withTitle/withTitle.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-import {Slide,  Typography, Divider, Grid} from "@material-ui/core";
+import {Slide, Typography, Divider, Grid} from "@material-ui/core";
 //import PropTypes from 'prop-types';
 /**
  * @deprecated default title bar
  */
 export class DefaultTitleBar extends React.PureComponent {
   render() {
-    const {title, subTitle, description, AIcon, hasIcon} = this.props;
+    const {title, subTitle, description, icon} = this.props;
+    const Icon=()=>icon;
     return (
       <div>
         <Grid container>
@@ -21,22 +22,22 @@ export class DefaultTitleBar extends React.PureComponent {
             style={{
             textAlign: "center"
           }}>
-            {hasIcon && <AIcon/>}
+            {icon && <Icon/>}
           </Grid>
 
           <Grid item lg={5} xl={5} md={5} sm={5} xs={5}>
             <div>
-              <Typography variant="title" color="primary" noWrap={true}>
-                ○ {title}
-              </Typography>
-              <Typography variant="subheading" color="textSecondary" noWrap={true} paragraph>
-                ♦ {subTitle}
-              </Typography>
+             {title && <Typography variant="title" color="primary" noWrap={true}>
+                 {title}
+              </Typography>}
+              {subTitle && <Typography variant="subheading" color="textSecondary" noWrap={true} paragraph>
+                 {subTitle}
+              </Typography>}
             </div>
           </Grid>
           <Grid item lg={6} xl={6} md={6} sm={5} xs={5}>
             <div>
-              <Typography
+            {description &&  <Typography
                 align="justify"
                 variant="body2"
                 paragraph
@@ -45,7 +46,7 @@ export class DefaultTitleBar extends React.PureComponent {
                 textIndent: "5pt"
               }}>
                 {description}
-              </Typography>
+              </Typography>}
             </div>
           </Grid>
         </Grid>
@@ -55,13 +56,9 @@ export class DefaultTitleBar extends React.PureComponent {
   }
 }
 
-// DefaultTitleBar.propTypes = {
-//   title: PropTypes.string,
-//   subTitle: PropTypes.string,
-//   description: PropTypes.string,
-//   AIcon: PropTypes.Component,
-//   hasIcon:PropTypes.object
-// }
+// DefaultTitleBar.propTypes = {   title: PropTypes.string,   subTitle:
+// PropTypes.string,   description: PropTypes.string,   AIcon:
+// PropTypes.Component,   hasIcon:PropTypes.object }
 
 /**
  *
@@ -69,49 +66,43 @@ export class DefaultTitleBar extends React.PureComponent {
  * @description an HOC for adding title to barteh routing component
  */
 
-export const withTitleHOC = (A, TitleBar) => {
+export const withTitle = bar=>(A) => {
   return class extends React.Component {
-    state = { in: false
+    state = { in: true
     };
-    componentDidMount() {
-      this.setState({in: true});
-    }
 
     render() {
-      const title = (A.context || {}).title || (A.component.context || {}).title;
-      const subTitle = (A.context || {}).subTitle || (A.component.context || {}).subTitle;
-      const pIcon = (A.context || {}).icon || (A.component.context || {}).icon;
+if(!bar)
+      return <A {...this.props}/>;
 
-      // || <span className="bt bt-activity bt-3x" />);
-      const AIcon = () => pIcon;
-      const description = (A.context || {}).description || (A.component.context || {}).description;
+      const context=bar || {};
+      const {title, subTitle, icon,description,TitleBar} = context;
+      const Title = TitleBar || DefaultTitleBar;
+      
+      
+      
 
-      const needToTitle = pIcon || description || title || subTitle;
-
+      const needToTitle = icon !== undefined || description !== undefined || title !== undefined || subTitle !== undefined;
       if (!needToTitle) 
-        return (
-          <Slide in={this.state. in} direction="left">
-            <A.component {...this.props}/>
-          </Slide>
-        );
+        return (<A {...this.props}/>);
       else 
         return (
+
           <div>
-            <Slide in={this.state. in} direction="right">
-              <TitleBar
+            <Slide in={this.state.in} direction="right">
+              <Title
                 title={title}
                 subTitle={subTitle}
                 description={description}
-                AIcon={AIcon}
-                hasIcon={pIcon}/></Slide>
+                icon={icon}/>
+                
+                </Slide>
             <div style={{
               marginTop: "10px"
             }}>
-              <Slide in={this.state. in} direction="up">
 
-                <A.component {...this.props}/>
+              <A {...this.props}/>
 
-              </Slide>
             </div>
           </div>
         );
@@ -119,24 +110,12 @@ export const withTitleHOC = (A, TitleBar) => {
     };
 };
 
-export function withTitleSingle(inroute, TitleBar) {
-  let out = {};
-  for (let o in inroute) {
-    if (o !== "component") {
-      out[o] = inroute[o];
-    }
-  }
+export default withTitle;
 
-  out.component = withTitleHOC(inroute, inroute.component.TitleBar || TitleBar || DefaultTitleBar);
-  return out;
-}
-
-export default function withTitle(routes, TitleBar) {
-  let out = [];
-  for (var i = 0; i < routes.length; i++) {
-    
-    out.push(withTitleSingle(routes[i], TitleBar));
-  }
-
-  return out;
-}
+// export function withTitleSingle(inroute, TitleBar) {   let out = {};   for
+// (let o in inroute) {     if (o !== "component") {       out[o] = inroute[o];
+//   }   }   out.component = withTitleHOC(inroute, inroute.component.TitleBar ||
+// TitleBar || DefaultTitleBar);   return out; } export default function
+// withTitle(routes, TitleBar) {   let out = [];   for (var i = 0; i <
+// routes.length; i++) {     out.push(withTitleSingle(routes[i], TitleBar));   }
+//   return out; }
